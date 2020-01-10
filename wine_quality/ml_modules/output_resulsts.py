ee4bac4a23c8bf720294a070b7d3326c3dbcea0b
Ric <cat.to.love418@gmail.com>
2020-01-11 01:36:26 +0900

    add wine_quality

---
+++ wine_quality/ml_modules/output_resulsts.py
@@ -0,0 +1,221 @@
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_auc_score, roc_curve, auc, accuracy_score
+from skater.model import InMemoryModel
+from skater.core.local_interpretation.lime.lime_tabular import LimeTabularExplainer
+from skater.core.explanations import Interpretation
+
+from scipy import interp
+
+def get_results(model, name, data, true_labels, gs=None, target_names = ['low', 'medium', 'high'], results=None, reasume=False):
+    best = 0
+    if hasattr(model, 'layers'):
+        param = model.history.params
+        best = np.mean(model.history.history['val_accuracy'])
+        predicted_labels = model.predict_classes(data) 
+        im_model = InMemoryModel(model.predict, examples=data, target_names=target_names)
+
+    else:
+        if gs != None:
+            param = gs.best_params_
+            best = gs.best_score_
+        predicted_labels = model.predict(data).ravel()
+        if hasattr(model, 'predict_proba'):
+            im_model = InMemoryModel(model.predict_proba, examples=data, target_names=target_names)
+        elif hasattr(model, 'decision_function'):
+            im_model = InMemoryModel(model.decision_function, examples=data, target_names=target_names)
+    if len(set(true_labels)) < len(set(predicted_labels)):
+        display_model_performance_metrics(true_labels, predicted_labels = predicted_labels, target_names = target_names)
+    else:
+        display_model_performance_metrics(true_labels, predicted_labels = predicted_labels, target_names = [str(x) for x in true_labels.unique()])
+    if len(target_names)==2:
+        ras = roc_auc_score(y_true=true_labels, y_score=predicted_labels)
+    else:
+        roc_auc_multiclass, ras = roc_auc_score_multiclass(y_true=true_labels, y_score=predicted_labels, target_names=target_names)
+        print('\nROC AUC Score by Classes:\n',roc_auc_multiclass)
+        print('-'*60)
+
+    print('\n\n              ROC AUC Score: {:2.2%}'.format(ras))
+    prob, score_roc, roc_auc = plot_model_roc_curve(model, data, true_labels, label_encoder=None, class_names=target_names)
+    
+    interpreter = Interpretation(data, feature_names=data.columns)
+    plots = interpreter.feature_importance.plot_feature_importance(im_model, progressbar=False, ascending=True)
+    
+    r1 = pd.DataFrame([(prob, best, np.round(accuracy_score(true_labels, predicted_labels), 4), 
+                         ras, roc_auc)], index = [name],
+                         columns = ['Prob', 'CV Accuracy', 'Accuracy', 'ROC AUC Score', 'ROC Area'])
+    if reasume:
+        results = r1
+    elif (name in results.index):        
+        results.loc[[name], :] = r1
+    else: 
+        results = results.append(r1)
+        
+    return results
+
+def roc_auc_score_multiclass(y_true, y_score, target_names, average = "macro"):
+
+  #creating a set of all the unique classes using the actual class list
+  unique_class = set(y_true)
+  roc_auc_dict = {}
+  mean_roc_auc = 0
+  for num, per_class in enumerate(unique_class):
+    #creating a list of all the classes except the current class 
+    other_class = [x for x in unique_class if x != per_class]
+
+    #marking the current class as 1 and all other classes as 0
+    new_y_true = [0 if x in other_class else 1 for x in y_true]
+    new_y_score = [0 if x in other_class else 1 for x in y_score]
+    num_new_y_true = sum(new_y_true)
+
+    #using the sklearn metrics method to calculate the roc_auc_score
+    roc_auc = roc_auc_score(new_y_true, new_y_score, average = average)
+    roc_auc_dict[target_names[num]] = np.round(roc_auc, 4)
+    mean_roc_auc += num_new_y_true * np.round(roc_auc, 4)
+    
+  mean_roc_auc = mean_roc_auc/len(y_true)  
+  return roc_auc_dict, mean_roc_auc
+
+def get_metrics(true_labels, predicted_labels):
+    
+    print('Accuracy:  {:2.2%} '.format(metrics.accuracy_score(true_labels, predicted_labels)))
+    print('Precision: {:2.2%} '.format(metrics.precision_score(true_labels, predicted_labels, average='weighted')))
+    print('Recall:    {:2.2%} '.format(metrics.recall_score(true_labels, predicted_labels, average='weighted')))
+    print('F1 Score:  {:2.2%} '.format(metrics.f1_score(true_labels, predicted_labels, average='weighted')))
+                        
+
+def train_predict_model(classifier,  train_features, train_labels,  test_features, test_labels):
+    # build model    
+    classifier.fit(train_features, train_labels)
+    # predict using model
+    predictions = classifier.predict(test_features) 
+    return predictions    
+
+
+def display_confusion_matrix(true_labels, predicted_labels, target_names):
+    
+    total_classes = len(target_names)
+    level_labels = [total_classes*[0], list(range(total_classes))]
+
+    cm = metrics.confusion_matrix(y_true=true_labels, y_pred=predicted_labels)
+    cm_frame = pd.DataFrame(data=cm, 
+                            columns=pd.MultiIndex(levels=[['Predicted:'], target_names], labels=level_labels), 
+                            index=pd.MultiIndex(levels=[['Actual:'], target_names], labels=level_labels)) 
+    print(cm_frame) 
+    
+def display_classification_report(true_labels, predicted_labels, target_names):
+
+    report = metrics.classification_report(y_true=true_labels, y_pred=predicted_labels, target_names=target_names) 
+    print(report)
+    
+def display_model_performance_metrics(true_labels, predicted_labels, target_names):
+    print('Model Performance metrics:')
+    print('-'*30)
+    get_metrics(true_labels=true_labels, predicted_labels=predicted_labels)
+    print('\nModel Classification report:')
+    print('-'*30)
+    display_classification_report(true_labels=true_labels, predicted_labels=predicted_labels, target_names=target_names)
+    print('\nPrediction Confusion Matrix:')
+    print('-'*30)
+    display_confusion_matrix(true_labels=true_labels, predicted_labels=predicted_labels, target_names=target_names)
+
+
+def plot_model_roc_curve(clf, features, true_labels, label_encoder=None, class_names=None):
+    
+    ## Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    if hasattr(clf, 'classes_'):
+        class_labels = clf.classes_
+    elif label_encoder:
+        class_labels = label_encoder.classes_
+    elif class_names:
+        class_labels = class_names
+    else:
+        raise ValueError('Unable to derive prediction classes, please specify class_names!')
+    n_classes = len(class_labels)
+   
+    if n_classes == 2:
+        if hasattr(clf, 'predict_proba'):
+            prb = clf.predict_proba(features)
+            if prb.shape[1] > 1:
+                y_score = prb[:, prb.shape[1]-1] 
+            else:
+                y_score = clf.predict(features).ravel()
+            prob = True
+        elif hasattr(clf, 'decision_function'):
+            y_score = clf.decision_function(features)
+            prob = False
+        else:
+            raise AttributeError("Estimator doesn't have a probability or confidence scoring system!")
+        
+        fpr, tpr, _ = roc_curve(true_labels, y_score)      
+        roc_auc = auc(fpr, tpr)
+
+        plt.plot(fpr, tpr, label='ROC curve (area = {0:3.2%})'.format(roc_auc), linewidth=2.5)
+        
+    elif n_classes > 2:
+        if  hasattr(clf, 'clfs_'):
+            y_labels = label_binarize(true_labels, classes=list(range(len(class_labels))))
+        else:
+            y_labels = label_binarize(true_labels, classes=class_labels)
+        if hasattr(clf, 'predict_proba'):
+            y_score = clf.predict_proba(features)
+            prob = True
+        elif hasattr(clf, 'decision_function'):
+            y_score = clf.decision_function(features)
+            prob = False
+        else:
+            raise AttributeError("Estimator doesn't have a probability or confidence scoring system!")
+            
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_labels[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        ## Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_labels.ravel(), y_score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+        ## Compute macro-average ROC curve and ROC area
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+        # Finally average it and compute AUC
+        mean_tpr /= n_classes
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+        ## Plot ROC curves
+        plt.figure(figsize=(6, 4))
+        plt.plot(fpr["micro"], tpr["micro"], label='micro-average ROC curve (area = {0:2.2%})'
+                       ''.format(roc_auc["micro"]), linewidth=3)
+
+        plt.plot(fpr["macro"], tpr["macro"], label='macro-average ROC curve (area = {0:2.2%})'
+                       ''.format(roc_auc["macro"]), linewidth=3)
+
+        for i, label in enumerate(class_names):
+            plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:2.2%})'
+                                           ''.format(label, roc_auc[i]), linewidth=2, linestyle=':')
+        roc_auc = roc_auc["macro"]   
+    else:
+        raise ValueError('Number of classes should be atleast 2 or more')
+        
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([-0.01, 1.0])
+    plt.ylim([0.0, 1.01])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+    
+    return prob, y_score, roc_auc
